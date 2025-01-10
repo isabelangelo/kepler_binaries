fd43aeadb9aa93030c30e5826897f6888b53d0a7
Isabel Angelo <isabelangelo@astro.ucla.edu>
2025-01-10 15:29:22 -0800

    save original spectra for single star + binary samples

--- load_single_validation_sample.py
+++ load_single_validation_sample.py
@@ -37,10 +37,10 @@ raghavan2010_singles = raghavan2010_singles[columns_to_keep].rename(columns=
 raghavan2010_singles.to_csv('./data/label_and_metric_dataframes/raghavan_single_labels.csv', index=False)
 
 
-# rsync HIRES spectra of Raghavan 2010 single stars ============================================== 
-# add row to match CKS obs_id row
+# # rsync HIRES spectra of Raghavan 2010 single stars ============================================== 
+# # add row to match CKS obs_id row
 # print('copying Raghavan 2010 single sample spectra from cadence')
-# raghavan2010_singles['obs_id'] = ['r'+i for i in raghavan2010_singles.observation_id]
+# raghavan2010_singles['obs_id'] = ['r'+i for i in raghavan2010_singles.lib_obs]
 # for index, row in raghavan2010_singles.iterrows():
 #     obs_ids = [row.observation_id.replace('rj','bj'), row.obs_id, row.obs_id.replace('rj','ij')]
 #     for obs_id in obs_ids:
@@ -57,7 +57,7 @@ raghavan2010_singles.to_csv('./data/label_and_metric_dataframes/raghavan_single_
 #     print('copied {} b,r,i chip spectra to ./data/raghavan2010_singles_spectra/'.format(row.resolvable_name))
 
 
-# shift and register spectra with specmatch-emp ========================================================
+# # shift and register spectra with specmatch-emp ========================================================
 
 # print('shifting and registering Raghavan 2010 single spectra for binary model validation')
 # raghavan2010_spectrum_ids = [i[37:-5] for i in glob.glob('./data/raghavan2010_singles_spectra/ij*.fits')]
@@ -80,65 +80,79 @@ raghavan2010_singles.to_csv('./data/label_and_metric_dataframes/raghavan_single_
 df_path = './data/spectrum_dataframes'
 shifted_path = './data/raghavan2010_singles_spectra_shifted'
 print('saving wavelet-filtered flux, sigma to dataframes')
-flux_df = pd.DataFrame()
-sigma_df = pd.DataFrame()
-for order_n in range(1,17):    
-	# lists to store data
-	id_starname_list = []
-	flux_list = []
-	sigma_list = []
 
-	# order index for wavelength re-scaling
-	order_idx = order_n - 1
+def flux_and_sigma_dataframes(filter_wavelets = True):
+	flux_df = pd.DataFrame()
+	sigma_df = pd.DataFrame()
+	for order_n in range(1,17):    
+		# lists to store data
+		id_starname_list = []
+		flux_list = []
+		sigma_list = []
+		# order index for wavelength re-scaling
+		order_idx = order_n - 1
 
-	# get order data for all stars in training set
-	for idx, row in raghavan2010_singles.iterrows():
-		# load file data
-		filename = '{}/{}_adj.fits'.format(
-			shifted_path,  
-			row.lib_obs)
-		id_starname_list.append(row.id_starname) # save star name for column
-		print(row.id_starname, end=', ')
+		# get order data for all stars in training set
+		for idx, row in raghavan2010_singles.iterrows():
+			# load file data
+			filename = '{}/{}_adj.fits'.format(
+				shifted_path,  
+				row.lib_obs)
+			id_starname_list.append(row.id_starname) # save star name for column
+			print(row.id_starname, end=', ')
 
-		# load spectrum from file
-		# and resample to unclipped HIRES wavelength scale
-		# (since flux, sigma arrays get clipped post-wavelet filtering)
-		KOI_spectrum = read_fits(filename)
-		rescaled_order = KOI_spectrum.rescale(original_wav_data[order_idx])
+			# load spectrum from file
+			# and resample to unclipped HIRES wavelength scale
+			# (since flux, sigma arrays get clipped post-wavelet filtering)
+			KOI_spectrum = read_fits(filename)
+			rescaled_order = KOI_spectrum.rescale(original_wav_data[order_idx])
 
-		# process for Cannon training
-		flux_norm, sigma_norm = dwt.load_spectrum(
-		    rescaled_order, 
-		    True) # perform wavelet filtering
+			# process for Cannon training
+			flux_norm, sigma_norm = dwt.load_spectrum(
+			    rescaled_order, 
+			    filter_wavelets) # if True, performs wavelet filtering
 
-		# save to lists
-		flux_list.append(flux_norm)
-		sigma_list.append(sigma_norm)
+			# save to lists
+			flux_list.append(flux_norm)
+			sigma_list.append(sigma_norm)
 
-	# store flux, sigma data
-	flux_df_n = pd.DataFrame(dict(zip(id_starname_list, flux_list)))
-	sigma_df_n = pd.DataFrame(dict(zip(id_starname_list, sigma_list)))
+		# store flux, sigma data
+		flux_df_n = pd.DataFrame(dict(zip(id_starname_list, flux_list)))
+		sigma_df_n = pd.DataFrame(dict(zip(id_starname_list, sigma_list)))
 
-	# store order number
-	flux_df_n.insert(0, 'order_number', order_n)
-	sigma_df_n.insert(0, 'order_number', order_n)
+		# store order number
+		flux_df_n.insert(0, 'order_number', order_n)
+		sigma_df_n.insert(0, 'order_number', order_n)
 
-	# save to final dataframe
-	flux_df = pd.concat([flux_df, flux_df_n])
-	sigma_df = pd.concat([sigma_df, sigma_df_n])
+		# generate final dataframe
+		flux_df = pd.concat([flux_df, flux_df_n])
+		sigma_df = pd.concat([sigma_df, sigma_df_n])
 
-# write flux, sigma to .csv files
-# NOTE: to save original flux, just change filter_wavelets in L90 and run this code.
-flux_path = '{}/raghavan_single_flux_dwt.csv'.format(df_path)
-sigma_path = '{}/raghavan_single_sigma_dwt.csv'.format(df_path)
+	return flux_df, sigma_df
 
-flux_df.to_csv(flux_path, index=False)
-sigma_df.to_csv(sigma_path, index=False)
+# write wavelet-filtered flux, sigma to .csv files
+flux_df_dwt, sigma_df_dwt = flux_and_sigma_dataframes(filter_wavelets = True)
+flux_path_dwt = '{}/raghavan_single_flux_dwt_TEST.csv'.format(df_path)
+sigma_path_dwt = '{}/raghavan_single_sigma_dwt_TEST.csv'.format(df_path)
+flux_df_dwt.to_csv(flux_path_dwt, index=False)
+sigma_df_dwt.to_csv(sigma_path_dwt, index=False)
 print('wavelet-filtered spectra saved to:')
-print(flux_path)
-print(sigma_path)
+print(flux_path_dwt)
+print(sigma_path_dwt)
+
+# write original flux, sigma to .csv files
+flux_df_original, sigma_df_original = flux_and_sigma_dataframes(filter_wavelets = False)
+flux_path_original = '{}/raghavan_single_flux_original_TEST.csv'.format(df_path)
+sigma_path_original = '{}/raghavan_single_sigma_original_TEST.csv'.format(df_path)
+flux_df_original.to_csv(flux_path_original, index=False)
+sigma_df_original.to_csv(sigma_path_original, index=False)
+print('original spectra saved to:')
+print(flux_path_original)
+print(sigma_path_original)
 
 
 
-
+df1 = pd.read_csv('./data/spectrum_dataframes/raghavan_single_sigma_dwt_TEST.csv')
+df2 = pd.read_csv('./data/spectrum_dataframes/raghavan_single_sigma_dwt.csv')
+False in df1==df2
 
